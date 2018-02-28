20d0fc90471a9c4b1e32706c7b9b6a9e148e533b
raphaelauv <raphaelauv@users.noreply.github.com>
2018-02-28 17:47:25 +0100

    Optimisation , find the best key between multiple possibilities

--- Crypto Vernam Weak Attack/VernamWeak.java
+++ Crypto Vernam Weak Attack/VernamWeak.java
@@ -12,9 +12,6 @@ import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-
-
-
 /*
  * Keep a score and a value of underKey
  */
@@ -34,7 +31,8 @@ class Couple implements Comparable<Couple>{
 		}else if(arg0.score<this.score) {
 			return 1;
 		}else {
-			return 0;
+			this.score+=-0.00000001d; //keep very close result
+			return 1;
 		}
 	}
 
@@ -60,6 +58,15 @@ class VernamWeak {
 	static int ASSCI_a = 97;
 	static int ASSCI_z = 122;
 	
+	
+	//not coherent
+	static int ASSCII_GUILL_MIN =8216;
+	static int ASSCII_GUILL_MAX =8220;
+	
+	public static int lenghKey = 4;
+	
+	private static int keepMaxValues = 20;
+	
 	public static String c(Key k, String t) {
 		StringBuilder b = new StringBuilder(t.length());
 		for (int i = 0; i < t.length(); i++) {
@@ -67,14 +74,18 @@ class VernamWeak {
 		}
 		return b.toString();
 	}
-	
-	public static int lenghKey = 4;
-
-	
+		
 	public static boolean txtIsInASCCI(String txt) {
+		char tmp;
+		
 		for(int i=0; i<txt.length();i++) {
-			if(txt.charAt(i)>limitASSCI_hight) {
-				return false;
+			tmp =txt.charAt(i); 
+			if(tmp >limitASSCI_hight) {
+				if(tmp< ASSCII_GUILL_MIN && tmp>ASSCII_GUILL_MAX) {
+					System.out.println(" char not assci "+(int) tmp + " "+ tmp);
+					return false;
+				}
+				
 			}
 		}
 		
@@ -117,6 +128,11 @@ class VernamWeak {
 		if (a<ASSCI_A) {
 			return -1;
 		}
+		
+		if( ASSCII_GUILL_MIN<=a && a<=ASSCII_GUILL_MAX) {
+			return -1;
+		}
+		
 		else if(a>=ASSCI_A && a<=ASSCI_Z ){
 			return a-ASSCI_A;
 		}else if(a>=ASSCI_a && a<=ASSCI_z) {
@@ -173,7 +189,7 @@ class VernamWeak {
 	}
 	
 	
-	private static int keepMaxValues = 5;
+	
 	
 	private static void addToThree(Couple cpl, TreeSet<Couple> set) {
 		
@@ -202,10 +218,35 @@ class VernamWeak {
 		}
 	}
 	
-	public static Key findKey(String txt) {
-
+	public static void printDoubleArray(int [][] allKeys) {
+		System.out.println("-------------");
+		for(int [] tmp : allKeys) {
+			System.out.println(Arrays.toString(tmp));	
+		}
+		System.out.println("-------------");
+	}
+	
+	public static int[][] getArrayFromArrayTreeSet(TreeSet<Couple> [] arrayOfBestUnderKeys){
 		
-		Couple [] arrays = new Couple[lenghKey];
+		int [][] allKeys = new int[lenghKey][];
+		
+		for(int i =0; i<lenghKey;i++) {
+			//System.out.print("[");
+			allKeys[i]=new int[arrayOfBestUnderKeys[i].size()];
+			int j=0;
+			for(Couple a: arrayOfBestUnderKeys[i]) {
+				//System.out.print((int)a.value+" ");
+				
+				allKeys[i][j]=a.value;
+				j++;
+			}
+			//System.out.println("]");
+		}
+		
+		return allKeys;
+	}
+	
+	public static int[][] findKeys(String txt) {
 		
 		TreeSet<Couple> [] arrayOfBestUnderKeys = new TreeSet[lenghKey];
 		for(int i=0; i<lenghKey;i++) {
@@ -215,11 +256,8 @@ class VernamWeak {
 		
 		String txtDecode="";
 		char key[] = new char[lenghKey];
-		Key k = new Key(key);
 		float [] arrayFreq;
 		double score;
-		double lastScore=1000;	
-		int lastGoodValue=0;
 		
 		for(int i=0; i<lenghKey;i++) {
 			for(int j=0; j<Character.MAX_VALUE; j++) {
@@ -228,7 +266,7 @@ class VernamWeak {
 				txtDecode = cWithPosition(txt,(char)j,i);
 
 				if(txtDecode!=null) {
-					//System.out.println("finded "+txtDecode);	
+					//System.out.println("finded ");	
 				}
 				
 				if(txtDecode==null) {
@@ -253,37 +291,21 @@ class VernamWeak {
 				
 				addToThree(new Couple(score,(char)j),arrayOfBestUnderKeys[i]);
 				
-				if(score<=lastScore) {
-					//System.out.println("pour  "+ (int) j +" better score "+score);
-					lastGoodValue=j;
-					lastScore=score;
-				}
 			}
 			
-			System.out.println("pour "+i+" on prends : "+(int)lastGoodValue+" avec score "+lastScore);
-			k.array[i]=(char)lastGoodValue;
-			lastGoodValue=-1;
-			lastScore=1000;
 		}
 		
-		for(int i =0; i<lenghKey;i++) {
-			System.out.print("[");
-			for(Couple a: arrayOfBestUnderKeys[i]) {
-				System.out.print((int)a.value+" ");
-			}
-			System.out.println("]");
-		}
-
-		return k;
+		int [][] allKeys= getArrayFromArrayTreeSet(arrayOfBestUnderKeys);
+		printDoubleArray(allKeys);
+		return allKeys;
 	}
 
 	public static void main(String[] args) throws IOException {
 		
 		Key k = g();
+		//Key k = new Key(new char[] {1,2,3,4});
 		System.out.println(k);
 		/*
-		
-		
 		String rst = c(k, "hello i will be in vacation tomorow");
 		System.out.println(rst);
 		String decoded = c(k, rst); 
@@ -292,30 +314,101 @@ class VernamWeak {
 		
 		String englishtxt = readFile("EnglishTxt",StandardCharsets.UTF_8);
 		
-		System.out.println("is an lATIN ALPHABET TEXTE ? "+txtIsInASCCI(englishtxt));
+		if(txtIsInASCCI(englishtxt)) {
+			System.out.println("is lATIN ALPHABET TEXTE");
+			
+		}else {
+			System.out.println("is not lATIN ALPHABET TEXTE");
+			return;
+		}
+		
+		
 	
 
 		HashSet<String> hash = getHashSet(1000, "wordsEN.txt");
 		
-		String encoedText = c(k,englishtxt);
+		String encodedText = c(k,englishtxt);
 		
 		
 		float resultFind =0f;
-		Key tmpKey;
+		//Key tmpKey;
 		String decodedText ="";
 	
+		int [][] allKeys = findKeys(encodedText);
 		
-		tmpKey =findKey(encoedText);
-		System.out.println(tmpKey);
-		decodedText = c(tmpKey,encoedText);
+		Key bestFind = getBetterKeyPossible(allKeys,encodedText,hash);
+		
+		decodedText = c(bestFind,encodedText);
 		resultFind = getEnglishScore(hash, decodedText);
-		System.out.println("score find "+resultFind);
+		System.out.println("score find "+resultFind+ " whith "+bestFind);
 		System.out.println("----------------------------------------------");
 		System.out.println(decodedText);
 		
 
 	}
 
+	
+	public static Key getBetterKeyPossible(int [][] allKeys, String encodedText ,HashSet<String> hash) {
+		
+		float resultFind;
+		float bestResultFind =0f;
+		Key bestKey= new Key(new char[lenghKey]);
+		
+		
+		char[]tmpArray=new char[lenghKey];
+		Key tmpKey=new Key(tmpArray);
+		
+		int [] actualIndexs = new int[lenghKey];
+		
+		boolean notFinish=true;
+		
+		
+		int cmp=0;
+		
+		String decodedText ="";
+			
+		while(notFinish) {
+
+			for(int i =0; i<tmpArray.length;i++) {
+				tmpArray[i]=(char) allKeys[i][actualIndexs[i]];
+			}
+			
+			//System.out.println(Arrays.toString(actualIndexs));
+			//System.out.println(tmpKey);
+			
+			do {
+				if(allKeys[cmp].length > actualIndexs[cmp] +1) {
+					actualIndexs[cmp]++;
+					break;
+				}
+				else {
+					actualIndexs[cmp]=0;
+					cmp++;
+					if(cmp==lenghKey) {
+						break;
+					}
+				}
+			}while(true);
+			if(cmp==lenghKey) {
+				break;
+			}
+			cmp=0;
+			
+			decodedText = c(tmpKey,encodedText);
+			
+			resultFind = getEnglishScore(hash, decodedText);
+			//System.out.println(resultFind+" "+tmpKey);
+			if(resultFind>bestResultFind) {
+				bestResultFind=resultFind;
+				System.arraycopy(tmpArray,0,bestKey.array,0,tmpArray.length);
+			}
+			
+		}
+		
+		return bestKey;
+		
+	}
+	
 	public static float getEnglishScore(HashSet<String> hash , String decodedText ) {
 		String[] arrayOfTxt;
 		int nbPresent = 0;
